f89997add51d67f857a8dc613bee942e33603cbf
htmujahid <htmujahid@gmail.com>
2026-06-03 06:46:49 +0500

    fix: remove unnecessary authentication requirement from TwoFactorPage

--- src/app/home/(navbar)/account/two-factor/page.tsx
+++ src/app/home/(navbar)/account/two-factor/page.tsx
@@ -1,9 +1,6 @@
 import { TwoFactorContainer } from '@/components/account/two-factor-container';
-import { requireAuth } from '@/orpc/proxy';
 
 export default async function TwoFactorPage() {
-  await requireAuth();
-
   return (
     <div className="flex w-full max-w-xl flex-col space-y-4 p-4">
       <TwoFactorContainer />
